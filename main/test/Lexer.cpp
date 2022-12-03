275f2838d55f363c6f8558a6eb982f395f442492
Ruletka <maksim.manezkiy.evg.99@gmail.com>
2022-12-03 12:48:12 +0300

    Create new `main/test` directory with test code

---
+++ main/test/Lexer.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file Lexer.cpp
+ * @author ruletkasuperstar
+ * @brief Lexical analysis
+ * 
+ * @date 03.12.2022
+ * @copyright ruletkasuperstar
+ * 
+ * Made with visual studio and g++
+ */
+
+namespace Test {
+  /**
+   * @class Lexer
+   * @brief All method to lexical analysis
+   */
+  class Lexer {};
+}
